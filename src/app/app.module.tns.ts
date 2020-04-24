f49b6d662a2034856dd75f9386c61456092b926a
unknown <jbambrick@tsilhqotinca>
2020-04-24 09:52:03 -0700

    first commit

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -4,6 +4,8 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { NavbarComponent } from '@src/app/navbar/navbar.component';
+import { MenuComponent } from '@src/app/menu/menu.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
@@ -16,6 +18,8 @@ import { HomeComponent } from '@src/app/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
+    MenuComponent,
   ],
   imports: [
     NativeScriptModule,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { NavbarComponent } from '@src/app/navbar/navbar.component';
+import { MenuComponent } from '@src/app/menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
